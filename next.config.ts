78299e6f078300ffecb42213137f9d4c2c693049
Maycow Melo <melo.maycow@gmail.com>
2025-03-04 07:31:59 -0300

    fix: remove storybook build

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withStorybook = process.env.STORYBOOK === "true";
-
 const withNextIntl = createNextIntlPlugin();
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-    return withStorybook
-      ? [
-          {
-            source: "/storybook/:path*",
-            destination: "/storybook-static/:path*",
-          },
-        ]
-      : [];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default withNextIntl(nextConfig);
