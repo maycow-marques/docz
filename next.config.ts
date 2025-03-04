1819aa816c516f24a2f855edb0cb3d6dc23905ee
Maycow Melo <melo.maycow@gmail.com>
2025-03-03 23:52:08 -0300

    feat: configure storybook deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
+const withStorybook = process.env.STORYBOOK === "true";
+
 const withNextIntl = createNextIntlPlugin();
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return withStorybook
+      ? [
+          {
+            source: "/storybook/:path*",
+            destination: "/storybook-static/:path*",
+          },
+        ]
+      : [];
+  },
+};
 
 export default withNextIntl(nextConfig);
